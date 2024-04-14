f2f68d9c045b298ac0fec9d121458bb8a5a31428
Lucca Zaghi <lucca.zaghi@gmail.com>
2024-04-14 12:54:18 -0300

    chore: inicia backend (typescript+sequelize)

---
+++ backend/src/database/config/database.ts
@@ -0,0 +1,12 @@
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '12345678',
+  database: process.env.DB_NAME || 'db_shopper',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3306,
+  dialect: 'mysql',
+}
+
+export = config;
