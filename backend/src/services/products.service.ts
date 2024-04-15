6e030eda3e02f2fa64df7be715c68fae7cca0824
Lucca Zaghi <lucca.zaghi@gmail.com>
2024-04-15 15:16:19 -0300

    feat: implementa validação de campos preenchidos e de novos preços seguindo as regras

--- backend/src/services/products.service.ts
+++ backend/src/services/products.service.ts
@@ -39,16 +39,51 @@ export default class ProductsService implements IProductsService {
         error: null,
       }
 
+      if(!product.product_code || !product.new_price) {
+        const invalidInputProduct = {
+          ...validProduct,
+          error: 'Os campos de código de produto e de novo preço são obrigatórios'
+        }
+        return invalidInputProduct;
+      }
+
       if(!dbProduct) {
-        let nonexistentProduct = {
+        const nonexistentProduct = {
           ...validProduct,
           error: 'Não existe produto com o código informado'
         }
         return nonexistentProduct;
       }
 
+      if(!this.validSalePrice(product.new_price, dbProduct.cost_price)) {
+        const invalidSalePriceProduct = {
+          ...validProduct,
+          error: 'O preço de venda não pode ser menor que o preço de custo'
+        }
+        return invalidSalePriceProduct;
+      }
+
+      if(!this.validReadjustment(product.new_price, +dbProduct.sales_price)) {
+        const invalidReadjustmentProduct = {
+          ...validProduct,
+          error: 'O reajuste não pode ser maior ou menor do que 10% do valor atual do produto'
+        }
+        return invalidReadjustmentProduct;
+      }
+
       return validProduct;
     })
     return validatedProcuts;
+  }
+
+  validSalePrice(newSalesPrice: number, costPrice: number) {
+    return newSalesPrice >= costPrice;
+  }
+
+  validReadjustment(newSalesPrice: number, currentPrice: number) {
+    const increment = +(currentPrice * 1.1).toFixed(2);
+    const decrement = +(currentPrice * 0.9).toFixed(2);
+
+    return newSalesPrice === increment || newSalesPrice === decrement;
   }
 }
