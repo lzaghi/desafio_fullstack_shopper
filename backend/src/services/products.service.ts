5e8d3c3f25e63bb799330ab7dccd80c086f5ce46
Lucca Zaghi <lucca.zaghi@gmail.com>
2024-04-20 11:44:33 -0300

    feat: implementa validação para reajustes de pacotes depender de reajustes válidos de seus produtos

--- backend/src/services/products.service.ts
+++ backend/src/services/products.service.ts
@@ -10,6 +10,148 @@ export default class ProductsService implements IProductsService {
     private readonly packsModel: typeof PacksModel
   ) {}
 
+  async updateProducts(inputProducts: IInputProduct[]): Promise<void> {
+    const validatedProcuts = await this.validateProducts(inputProducts);
+
+    const validInput = validatedProcuts.every((product: IValidatedProduct) => !product.error);
+    
+    if(validInput) {
+      await Promise.all(inputProducts.map(async (product) => {
+        await this.productsModel.update(
+          { sales_price: product.new_price },
+          { where: { code: product.product_code } }
+        )
+      }));
+    } else {
+      throw new BadRequestError('Invalid input');
+    }
+  }
+
+  async validateProducts(inputProducts: IInputProduct[]): Promise<IValidatedProduct[]> {
+    const dbProducts = await this.getProducts(inputProducts) as IHashProducts;
+
+    const hashInputProducts = this.hashFromArray(inputProducts, 'product_code');
+    const duplicates = this.checkForDuplicates(inputProducts);
+
+    const invalidProducts: IValidatedProduct[] = [];
+    
+    const validatedProcuts = inputProducts
+      .sort((a, b) => +a.product_code - +b.product_code)
+      .map((product) => {
+        const dbProduct = dbProducts[product.product_code];
+
+        const validProduct = {
+          product_code: product.product_code,
+          name: dbProduct?.name,
+          current_price: dbProduct?.sales_price,
+          new_price: product.new_price,
+          error: null,
+        }
+
+        if(duplicates.includes(product.product_code)) {
+          const invalidDuplicatedProduct = {
+            ...validProduct,
+            error: 'Produto a ser reajustado está duplicado no arquivo de entrada'
+          }
+          invalidProducts.push(invalidDuplicatedProduct);
+          return invalidDuplicatedProduct;
+        }
+
+        if(!product.product_code || !product.new_price) {
+          const invalidInputProduct = {
+            ...validProduct,
+            error: 'Os campos de código de produto e de novo preço são obrigatórios'
+          }
+          invalidProducts.push(invalidInputProduct);
+          return invalidInputProduct;
+        }
+
+        if(!this.validPriceFormat(product.new_price)) {
+          const invalidPriceFormatProduct = {
+            ...validProduct,
+            error: "O novo preço deve ser um número positivo com até duas casas decimais, separadas por '.'"
+          }
+          invalidProducts.push(invalidPriceFormatProduct);
+          return invalidPriceFormatProduct;
+        }
+
+        if(!dbProduct) {
+          const nonexistentProduct = {
+            ...validProduct,
+            error: 'Não existe produto com o código informado'
+          }
+          invalidProducts.push(nonexistentProduct);
+          return nonexistentProduct;
+        }
+
+        if(!this.validPackAssociation(dbProduct, dbProducts)) {
+          const invalidPackAssociationProduct = {
+            ...validProduct,
+            error: 'Produtos associados a esse também devem ser reajustados'
+          }
+          invalidProducts.push(invalidPackAssociationProduct);
+          return invalidPackAssociationProduct;
+        }
+
+        if(!this.validSalePrice(+product.new_price, +dbProduct.cost_price)) {
+          const invalidSalePriceProduct = {
+            ...validProduct,
+            error: 'O preço de venda não pode ser menor que o preço de custo'
+          }
+          invalidProducts.push(invalidSalePriceProduct);
+          return invalidSalePriceProduct;
+        }
+
+        if(!this.validReadjustment(+product.new_price, dbProduct)) {
+          const invalidReadjustmentProduct = {
+            ...validProduct,
+            error: 'O reajuste não pode ser maior ou menor do que 10% do valor atual do produto'
+          }
+          invalidProducts.push(invalidReadjustmentProduct);
+          return invalidReadjustmentProduct;
+        }
+
+        if(!this.validPrackProducts(dbProduct, invalidProducts)) {
+          const invalidPackProductsProduct = {
+            ...validProduct,
+            error: 'Produtos associados a esse pacote apresentam reajustes inválidos'
+          }
+          return invalidPackProductsProduct;
+        }
+
+        if(!this.validPriceAssociation(product, dbProduct, dbProducts, hashInputProducts)) {
+          const invalidPriceAssociationProduct = {
+            ...validProduct,
+            error: 'O valor do reajuste de pacotes deve corresponder ao reajuste dos produtos associados'
+          }
+          return invalidPriceAssociationProduct;
+        }
+
+        return validProduct;
+      })
+  
+    return validatedProcuts;
+  }
+
+  private hashFromArray(array: any[], key: string): { [key: string]: any } {
+    return array.reduce((acc, curr) => {
+      acc[curr[key]] = curr;
+      return acc;
+    }, {} as { [key: string]: any });
+  }
+
+  private checkForDuplicates(inputProducts: IInputProduct[]): number[] {
+    const seen = {} as { [key: string]: boolean };
+    const duplicates = [];
+    for(const product of inputProducts) {
+      if(seen[product.product_code]) {
+        duplicates.push(product.product_code);
+      }
+      seen[product.product_code] = true;
+    }
+    return duplicates;
+  }
+
   private async getProducts(inputProducts: IInputProduct[]): Promise<IHashProducts> {
     const productCodes = inputProducts.map((product) => product.product_code);
     const products = await this.productsModel.findAll({
@@ -34,93 +176,6 @@ export default class ProductsService implements IProductsService {
     const hashDbProducts = this.hashFromArray(products, 'code');
 
     return hashDbProducts;
-  }
-
-  async validateProducts(inputProducts: IInputProduct[]): Promise<IValidatedProduct[]> {
-    const dbProducts = await this.getProducts(inputProducts) as IHashProducts;
-
-    const hashInputProducts = this.hashFromArray(inputProducts, 'product_code');
-    const duplicates = this.checkForDuplicates(inputProducts);
-    
-    const validatedProcuts = inputProducts.map((product) => {
-      const dbProduct = dbProducts[product.product_code];
-
-      const validProduct = {
-        product_code: product.product_code,
-        name: dbProduct?.name,
-        current_price: dbProduct?.sales_price,
-        new_price: product.new_price,
-        error: null,
-      }
-
-      if(duplicates.includes(product.product_code)) {
-        const invalidDuplicatedProduct = {
-          ...validProduct,
-          error: 'Produto a ser reajustado está duplicado no arquivo de entrada'
-        }
-        return invalidDuplicatedProduct;
-      }
-
-      if(!product.product_code || !product.new_price) {
-        const invalidInputProduct = {
-          ...validProduct,
-          error: 'Os campos de código de produto e de novo preço são obrigatórios'
-        }
-        return invalidInputProduct;
-      }
-
-      if(!this.validPriceFormat(product.new_price)) {
-        const invalidPriceFormatProduct = {
-          ...validProduct,
-          error: "O novo preço deve ser um número positivo com até duas casas decimais, separadas por '.'"
-        }
-        return invalidPriceFormatProduct;
-      }
-
-      if(!dbProduct) {
-        const nonexistentProduct = {
-          ...validProduct,
-          error: 'Não existe produto com o código informado'
-        }
-        return nonexistentProduct;
-      }
-
-      if(!this.validPackAssociation(dbProduct, dbProducts)) {
-        const invalidPackAssociationProduct = {
-          ...validProduct,
-          error: 'Produtos associados a esse também devem ser reajustados'
-        }
-        return invalidPackAssociationProduct;
-      }
-
-      if(!this.validSalePrice(+product.new_price, +dbProduct.cost_price)) {
-        const invalidSalePriceProduct = {
-          ...validProduct,
-          error: 'O preço de venda não pode ser menor que o preço de custo'
-        }
-        return invalidSalePriceProduct;
-      }
-
-      if(!this.validReadjustment(+product.new_price, dbProduct)) {
-        const invalidReadjustmentProduct = {
-          ...validProduct,
-          error: 'O reajuste não pode ser maior ou menor do que 10% do valor atual do produto'
-        }
-        return invalidReadjustmentProduct;
-      }
-
-      if(!this.validPriceAssociation(product, dbProduct, dbProducts, hashInputProducts)) {
-        const invalidPriceAssociationProduct = {
-          ...validProduct,
-          error: 'O valor do reajuste de pacotes deve corresponder ao reajuste dos produtos associados'
-        }
-        return invalidPriceAssociationProduct;
-      }
-
-      return validProduct;
-    })
-  
-    return validatedProcuts;
   }
 
   private validPriceFormat(price: number): boolean {
@@ -159,6 +214,18 @@ export default class ProductsService implements IProductsService {
     }
     return true;
   }
+  
+  private validPrackProducts(dbProduct: IProduct, invalidProducts: IValidatedProduct[]): boolean {
+    if(dbProduct.hasProducts!.length) {
+      for(const product of dbProduct.hasProducts!) {
+        const packProduct = invalidProducts.find((p) => +p.product_code === +product.product_id);
+        if(packProduct) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 
   private validPriceAssociation(product: IInputProduct, dbProduct: IProduct, dbProducts: IHashProducts, inputProducts: IHashInputProducts): boolean {
     if(dbProduct.hasProducts!.length) {
@@ -176,41 +243,5 @@ export default class ProductsService implements IProductsService {
       return packReadjustment.toFixed(2) === productsReadjustment.toFixed(2);
     }
     return true;
-  }
-
-  private hashFromArray(array: any[], key: string): { [key: string]: any } {
-    return array.reduce((acc, curr) => {
-      acc[curr[key]] = curr;
-      return acc;
-    }, {} as { [key: string]: any });
-  }
-
-  private checkForDuplicates(inputProducts: IInputProduct[]): number[] {
-    const seen = {} as { [key: string]: boolean };
-    const duplicates = [];
-    for(const product of inputProducts) {
-      if(seen[product.product_code]) {
-        duplicates.push(product.product_code);
-      }
-      seen[product.product_code] = true;
-    }
-    return duplicates;
-  }
-
-  async updateProducts(inputProducts: IInputProduct[]): Promise<void> {
-    const validatedProcuts = await this.validateProducts(inputProducts);
-
-    const validInput = validatedProcuts.every((product: IValidatedProduct) => !product.error);
-    
-    if(validInput) {
-      await Promise.all(inputProducts.map(async (product) => {
-        await this.productsModel.update(
-          { sales_price: product.new_price },
-          { where: { code: product.product_code } }
-        )
-      }));
-    } else {
-      throw new BadRequestError('Invalid input');
-    }
   }
 }
